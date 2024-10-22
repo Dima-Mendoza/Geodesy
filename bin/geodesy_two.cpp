e7870498b5575447e720e6fd21362da92d0a1486
Dima-Mendoza <spartanskijmapper534@gmail.com>
2024-10-23 00:53:36 +0300

    geodesy

--- bin/geodesy_two.cpp
+++ bin/geodesy_two.cpp
@@ -40,11 +40,18 @@ int main() {
     long double m_2_krosvskogo = (3.0/2.0)*e_1_krosvskogo*m_0_krosvskogo;
     long double m_4_krosvskogo = (5.0/4.0)*e_1_krosvskogo*m_2_krosvskogo;
     long double m_6_krosvskogo = (7.0/6.0)*e_1_krosvskogo*m_4_krosvskogo;
-
     long double m_0_wgs84 = wgs84.semi_major_axis*(1 - e_1_wgs84);
     long double m_2_wgs84 = (3.0/2.0)*e_1_wgs84*m_0_wgs84;
     long double m_4_wgs84 = (5.0/4.0)*e_1_wgs84*m_2_wgs84;
     long double m_6_wgs84 = (7.0/6.0)*e_1_wgs84*m_4_wgs84;
+    long double m_0_pz90 = pz90.semi_major_axis*(1 - e_1_pz90);
+    long double m_2_pz90 = (3.0/2.0)*e_1_pz90*m_0_pz90;
+    long double m_4_pz90 = (5.0/4.0)*e_1_pz90*m_2_pz90;
+    long double m_6_pz90 = (7.0/6.0)*e_1_pz90*m_4_pz90;
+    long double m_0_gsk2011 = gsk2011.semi_major_axis*(1 - e_1_gsk2011);
+    long double m_2_gsk2011 = (3.0/2.0)*e_1_gsk2011*m_0_gsk2011;
+    long double m_4_gsk2011 = (5.0/4.0)*e_1_gsk2011*m_2_gsk2011;
+    long double m_6_gsk2011 = (7.0/6.0)*e_1_gsk2011*m_4_gsk2011;
 
     //Коэффициенты a
     long double a_0_jgd2000 = m_0_jgd2000 + (m_2_jgd2000/2) + (3.0/8.0)*m_4_jgd2000;
@@ -55,8 +62,20 @@ int main() {
     long double a_2_krosvskogo = (m_2_krosvskogo/2.0) + (m_4_krosvskogo/2.0) + (15.0/32.0)*m_6_krosvskogo;
     long double a_4_krosvskogo = (m_4_krosvskogo/8.0) + (3.0/16.0)*m_6_krosvskogo;
     long double a_6_krosvskogo = (m_6_krosvskogo/32.0);
+    long double a_0_gsk2011 = m_0_gsk2011 + (m_2_gsk2011 / 2.0) + (3.0 / 8.0) * m_4_gsk2011;
+    long double a_2_gsk2011 = (m_2_gsk2011 / 2.0) + (m_4_gsk2011 / 2.0) + (15.0 / 32.0) * m_6_gsk2011;
+    long double a_4_gsk2011 = (m_4_gsk2011 / 8.0) + (3.0 / 16.0) * m_6_gsk2011;
+    long double a_6_gsk2011 = (m_6_gsk2011 / 32.0);
+    long double a_0_pz90 = m_0_pz90 + (m_2_pz90 / 2.0) + (3.0 / 8.0) * m_4_pz90;
+    long double a_2_pz90 = (m_2_pz90 / 2.0) + (m_4_pz90 / 2.0) + (15.0 / 32.0) * m_6_pz90;
+    long double a_4_pz90 = (m_4_pz90 / 8.0) + (3.0 / 16.0) * m_6_pz90;
+    long double a_6_pz90 = (m_6_pz90 / 32.0);
+    long double a_0_wgs84 = m_0_wgs84 + (m_2_wgs84 / 2.0) + (3.0 / 8.0) * m_4_wgs84;
+    long double a_2_wgs84 = (m_2_wgs84 / 2.0) + (m_4_wgs84 / 2.0) + (15.0 / 32.0) * m_6_wgs84;
+    long double a_4_wgs84 = (m_4_wgs84 / 8.0) + (3.0 / 16.0) * m_6_wgs84;
+    long double a_6_wgs84 = (m_6_wgs84 / 32.0);
 
-    //X
+    //X 1.1
 
     long double X_1_jgd2000 = a_0_jgd2000 * angle_in_rad 
         - (a_2_jgd2000 / 2) * sin(2 * angle_in_rad) 
@@ -78,17 +97,69 @@ int main() {
         + (a_4_krosvskogo / 4) * sin(4 * angle_in_rad_2) 
         - (a_6_krosvskogo / 6) * sin(6 * angle_in_rad_2);
 
+    long double X_1_pz90 = a_0_pz90 * angle_in_rad 
+        - (a_2_pz90 / 2) * sin(2 * angle_in_rad) 
+        + (a_4_pz90 / 4) * sin(4 * angle_in_rad) 
+        - (a_6_pz90 / 6) * sin(6 * angle_in_rad);
+
+    long double X_2_pz90 = a_0_pz90 * angle_in_rad_2 
+        - (a_2_pz90 / 2) * sin(2 * angle_in_rad_2) 
+        + (a_4_pz90 / 4) * sin(4 * angle_in_rad_2) 
+        - (a_6_pz90 / 6) * sin(6 * angle_in_rad_2);
+
+    long double X_1_wgs84 = a_0_wgs84 * angle_in_rad 
+        - (a_2_wgs84 / 2) * sin(2 * angle_in_rad) 
+        + (a_4_wgs84 / 4) * sin(4 * angle_in_rad) 
+        - (a_6_wgs84 / 6) * sin(6 * angle_in_rad);
+
+    long double X_2_wgs84 = a_0_wgs84 * angle_in_rad_2 
+        - (a_2_wgs84 / 2) * sin(2 * angle_in_rad_2) 
+        + (a_4_wgs84 / 4) * sin(4 * angle_in_rad_2) 
+        - (a_6_wgs84 / 6) * sin(6 * angle_in_rad_2);
+
+    long double X_1_gsk2011 = a_0_gsk2011 * angle_in_rad 
+        - (a_2_gsk2011 / 2) * sin(2 * angle_in_rad) 
+        + (a_4_gsk2011 / 4) * sin(4 * angle_in_rad) 
+        - (a_6_gsk2011 / 6) * sin(6 * angle_in_rad);
+
+    long double X_2_gsk2011 = a_0_gsk2011 * angle_in_rad_2 
+        - (a_2_gsk2011 / 2) * sin(2 * angle_in_rad_2) 
+        + (a_4_gsk2011 / 4) * sin(4 * angle_in_rad_2) 
+        - (a_6_gsk2011 / 6) * sin(6 * angle_in_rad_2);
+
+
+
+
     //long double X_1_jgd2000 = a_0_jgd2000*angle_in_rad- (a_2_jgd2000/2)*sin_b*2 + (a_4_jgd2000/4)*sin_b*4 - (a_6_jgd2000/6)*sin_b*6;
     //long double X_2_jgd2000 = a_0_jgd2000*angle_in_rad_2- (a_2_jgd2000/2)*sin_b_2*2 + (a_4_jgd2000/4)*sin_b_2*4 - (a_6_jgd2000/6)*sin_b_2*6;
     long double delta_X_jgd2000 = X_2_jgd2000 - X_1_jgd2000;
     //long double X_1_krosvskogo = a_0_krosvskogo*angle_in_rad - (a_2_krosvskogo/2)*sin_b*2 + (a_4_krosvskogo/4)*sin_b*4 - (a_6_krosvskogo/6)*sin_b*6;
     //long double X_2_krosvskogo = a_0_krosvskogo*angle_in_rad_2- (a_2_krosvskogo/2)*sin_b_2*2 + (a_4_krosvskogo/4)*sin_b_2*4 - (a_6_krosvskogo/6)*sin_b_2*6;
     long double delta_X_krosvskogo = X_2_krosvskogo - X_1_krosvskogo;
+    long double delta_X_gsk2011 = X_2_gsk2011 - X_1_gsk2011;
+    long double delta_X_pz90 = X_2_pz90 - X_1_pz90;
+    long double delta_X_wgs84 = X_2_wgs84 - X_1_wgs84;
 
 
 
     cout << fixed << setprecision(4) << delta_X_jgd2000 << endl;
     cout << fixed << setprecision(4) << delta_X_krosvskogo << endl;
+    cout << fixed << setprecision(4) << delta_X_gsk2011 << endl;
+    cout << fixed << setprecision(4) << delta_X_pz90 << endl;
+    cout << fixed << setprecision(4) << delta_X_wgs84 << endl;
+
+    //1.2
+
+    //Коэф v для вычисления радиуса кривизны
+    long double v_krosvskogo_2 = 1 + e_2_krosvskogo*pow(cos_b_2,2);
+    long double v_wgs84_2 = 1 + e_2_wgs84*pow(cos_b_2,2);
+    long double v_pz90_2 = 1 + e_2_pz90*pow(cos_b_2,2);
+    long double v_gsk2011_2 = 1 + e_2_gsk2011*pow(cos_b_2,2);
+    long double v_jgd2000_2 = 1 + e_2_jgd2000*pow(cos_b_2,2);
+
+    //TO--DO добавить значение M2 и выяснить как вычилить Mср
+
+
 
     // krosvskogo wgs84 pz90 gsk2011 jgd2000
 
