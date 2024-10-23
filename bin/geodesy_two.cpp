15c5e9e84176a12b7d20d5c96cec2265d83dfcdc
Dima-Mendoza <spartanskijmapper534@gmail.com>
2024-10-23 23:42:20 +0300

    geodesy

--- bin/geodesy_two.cpp
+++ bin/geodesy_two.cpp
@@ -3,10 +3,6 @@
 #include <xlsxwriter.h>
 #include "geo_two_h.h"
 
-//To-Do
-//РАЗОБРАТЬСЯ С ФОРМУЛОЙ ДЛЯ ИКС А ИМЕННО УМНОЖЕНИЕ ЧИСЛА НА ГРУДУСНУЮ ВЕЛЕЧИНУ
-//ВОЗМОЖНО РАЗБИТЬ ФОРМУЛУ НА 3 СОСТАВНЫЕ ЧАСТИ
-
 int main() {
 
     double B_2[3] = {57, 36, 0 };
@@ -29,6 +25,10 @@ int main() {
     long double cos_b_2 = cos(angle_in_rad_2);
     long double sin_l_2 = sin(L_ang_in_rad_2);
     long double cos_l_2 = cos(L_ang_in_rad_2);
+    long double angle_in_rad_avg = (angle_in_rad + angle_in_rad_2) / 2.0;
+    long double cos_b_avg = cos(angle_in_rad_avg);
+    long double delta_B = angle_in_rad_2 - angle_in_rad;
+    long double delta_L = 2.0 * M_PI/180.0;
 
 
     //Коэффиценты m
@@ -141,12 +141,13 @@ int main() {
     long double delta_X_wgs84 = X_2_wgs84 - X_1_wgs84;
 
 
-
+/*
     cout << fixed << setprecision(4) << delta_X_jgd2000 << endl;
     cout << fixed << setprecision(4) << delta_X_krosvskogo << endl;
     cout << fixed << setprecision(4) << delta_X_gsk2011 << endl;
     cout << fixed << setprecision(4) << delta_X_pz90 << endl;
     cout << fixed << setprecision(4) << delta_X_wgs84 << endl;
+*/
 
     //1.2
 
@@ -156,8 +157,132 @@ int main() {
     long double v_pz90_2 = 1 + e_2_pz90*pow(cos_b_2,2);
     long double v_gsk2011_2 = 1 + e_2_gsk2011*pow(cos_b_2,2);
     long double v_jgd2000_2 = 1 + e_2_jgd2000*pow(cos_b_2,2);
+    long double v_krosvskogo_avg = 1 + e_2_krosvskogo*pow(cos_b_avg,2);
+    long double v_wgs84_avg = 1 + e_2_wgs84 * pow(cos_b_avg, 2);
+    long double v_pz90_avg = 1 + e_2_pz90 * pow(cos_b_avg, 2);
+    long double v_gsk2011_avg = 1 + e_2_gsk2011 * pow(cos_b_avg, 2);
+    long double v_jgd2000_avg = 1 + e_2_jgd2000 * pow(cos_b_avg, 2);
 
-    //TO--DO добавить значение M2 и выяснить как вычилить Mср
+
+    //M2
+    long double M_2_krosvskogo = c_krosvskogo / pow(sqrt(v_krosvskogo_2),3);
+    long double M_2_wgs84 = c_wgs84 / pow(sqrt(v_wgs84_2), 3);
+    long double M_2_pz90 = c_pz90 / pow(sqrt(v_pz90_2), 3);
+    long double M_2_gsk2011 = c_gsk2011 / pow(sqrt(v_gsk2011_2), 3);
+    long double M_2_jgd2000 = c_jgd2000 / pow(sqrt(v_jgd2000_2), 3);
+
+    //Mср
+    long double M_avg_krosvskogo = c_krosvskogo / pow(sqrt(v_krosvskogo_avg),3);
+    long double M_avg_wgs84 = c_wgs84 / pow(sqrt(v_wgs84_avg), 3);
+    long double M_avg_pz90 = c_pz90 / pow(sqrt(v_pz90_avg), 3);
+    long double M_avg_gsk2011 = c_gsk2011 / pow(sqrt(v_gsk2011_avg), 3);
+    long double M_avg_jgd2000 = c_jgd2000 / pow(sqrt(v_jgd2000_avg), 3);
+
+    long double delta_X_krosvskogo_2 = (delta_B/6.0)* (M_krosvskogo + 4 * M_avg_krosvskogo + M_2_krosvskogo);
+    long double delta_X_wgs84_2 = (delta_B / 6.0) * (M_wgs84 + 4 * M_avg_wgs84 + M_2_wgs84);
+    long double delta_X_pz90_2 = (delta_B / 6.0) * (M_pz90 + 4 * M_avg_pz90 + M_2_pz90);
+    long double delta_X_gsk2011_2 = (delta_B / 6.0) * (M_gsk2011 + 4 * M_avg_gsk2011 + M_2_gsk2011);
+    long double delta_X_jgd2000_2 = (delta_B / 6.0) * (M_jgd2000 + 4 * M_avg_jgd2000 + M_2_jgd2000);
+
+    long double N_krosvskogo_2 = c_krosvskogo / sqrt(v_krosvskogo_2);
+    long double N_wgs84_2 = c_wgs84 / sqrt(v_wgs84_2);
+    long double N_pz90_2 = c_pz90 / sqrt(v_pz90_2);
+    long double N_gsk2011_2 = c_gsk2011 / sqrt(v_gsk2011_2);
+    long double N_jgd2000_2 = c_jgd2000 / sqrt(v_jgd2000_2);
+
+    long double delta_Y_1_krosvskogo = N_krosvskogo * cos_b * delta_L;
+    long double delta_Y_2_krosvskogo = N_krosvskogo_2 * cos_b_2 * delta_L;
+    long double delta_Y_1_wgs84 = N_wgs84 * cos_b * delta_L;
+    long double delta_Y_2_wgs84 = N_wgs84_2 * cos_b_2 * delta_L;
+    long double delta_Y_1_pz90 = N_pz90 * cos_b * delta_L;
+    long double delta_Y_2_pz90 = N_pz90_2 * cos_b_2 * delta_L;
+    long double delta_Y_1_gsk2011 = N_gsk2011 * cos_b * delta_L;
+    long double delta_Y_2_gsk2011 = N_gsk2011_2 * cos_b_2 * delta_L;
+    long double delta_Y_1_jgd2000 = N_jgd2000 * cos_b * delta_L;
+    long double delta_Y_2_jgd2000 = N_jgd2000_2 * cos_b_2 * delta_L;
+/*
+    cout << fixed << setprecision(4) 
+        << delta_Y_1_krosvskogo << " : " 
+        << delta_Y_2_krosvskogo << endl;
+    cout << fixed << setprecision(4) 
+        << delta_Y_1_wgs84 << " : " 
+        << delta_Y_2_wgs84 << endl;
+
+    cout << fixed << setprecision(4) 
+        << delta_Y_1_pz90 << " : " 
+        << delta_Y_2_pz90 << endl;
+
+    cout << fixed << setprecision(4) 
+        << delta_Y_1_gsk2011 << " : " 
+        << delta_Y_2_gsk2011 << endl;
+
+    cout << fixed << setprecision(4) 
+        << delta_Y_1_jgd2000 << " : " 
+        << delta_Y_2_jgd2000 << endl;
+*/
+
+    long double sr_M_1_krosvskogo = delta_X_krosvskogo_2 / 2.0 / 1000.0;
+    long double sr_M_2_krosvskogo = sr_M_1_krosvskogo / 60.0 * 1000;
+    long double sr_M_3_krosvskogo = sr_M_2_krosvskogo / 60.0;
+
+    long double rm_P_1_krosvskogo = delta_Y_1_krosvskogo / 2.0 / 1000.0;
+    long double rm_P_2_krosvskogo = rm_P_1_krosvskogo / 60.0 * 1000;
+    long double rm_P_3_krosvskogo = delta_Y_1_krosvskogo / 60.0;
+
+    long double sr_M_1_wgs84 = delta_X_wgs84_2 / 2.0 / 1000.0;
+    long double sr_M_2_wgs84 = sr_M_1_wgs84 / 60.0 * 1000;
+    long double sr_M_3_wgs84 = sr_M_2_wgs84 / 60.0;
+
+    long double rm_P_1_wgs84 = delta_Y_1_wgs84 / 2.0 / 1000.0;
+    long double rm_P_2_wgs84 = rm_P_1_wgs84 / 60.0 * 1000;
+    long double rm_P_3_wgs84 = delta_Y_1_wgs84 / 60.0;
+
+    long double sr_M_1_pz90 = delta_X_pz90_2 / 2.0 / 1000.0;
+    long double sr_M_2_pz90 = sr_M_1_pz90 / 60.0 * 1000;
+    long double sr_M_3_pz90 = sr_M_2_pz90 / 60.0;
+
+    long double rm_P_1_pz90 = delta_Y_1_pz90 / 2.0 / 1000.0;
+    long double rm_P_2_pz90 = rm_P_1_pz90 / 60.0 * 1000;
+    long double rm_P_3_pz90 = delta_Y_1_pz90 / 60.0;
+
+    long double sr_M_1_gsk2011 = delta_X_gsk2011_2 / 2.0 / 1000.0;
+    long double sr_M_2_gsk2011 = sr_M_1_gsk2011 / 60.0 * 1000;
+    long double sr_M_3_gsk2011 = sr_M_2_gsk2011 / 60.0;
+
+    long double rm_P_1_gsk2011 = delta_Y_1_gsk2011 / 2.0 / 1000.0;
+    long double rm_P_2_gsk2011 = rm_P_1_gsk2011 / 60.0 * 1000;
+    long double rm_P_3_gsk2011 = delta_Y_1_gsk2011 / 60.0;
+
+    long double sr_M_1_jgd2000 = delta_X_jgd2000_2 / 2.0 / 1000.0;
+    long double sr_M_2_jgd2000 = sr_M_1_jgd2000 / 60.0 * 1000;
+    long double sr_M_3_jgd2000 = sr_M_2_jgd2000 / 60.0;
+
+    long double rm_P_1_jgd2000 = delta_Y_1_jgd2000 / 2.0 / 1000.0;
+    long double rm_P_2_jgd2000 = rm_P_1_jgd2000 / 60.0 * 1000;
+    long double rm_P_3_jgd2000 = delta_Y_1_jgd2000 / 60.0;
+
+    cout << fixed << setprecision(3) << sr_M_1_krosvskogo << " : " << sr_M_1_jgd2000 << " : " << sr_M_1_gsk2011
+        << " : " << sr_M_1_pz90 << " : " << sr_M_1_wgs84 << endl;
+    
+    cout << fixed << setprecision(3) 
+        << sr_M_2_krosvskogo << " : " 
+        << sr_M_2_jgd2000 << " : " 
+        << sr_M_2_gsk2011 << " : " 
+        << sr_M_2_pz90 << " : " 
+        << sr_M_2_wgs84 << endl;
+
+    cout << fixed << setprecision(3) 
+        << sr_M_3_krosvskogo << " : " 
+        << sr_M_3_jgd2000 << " : " 
+        << sr_M_3_gsk2011 << " : " 
+        << sr_M_3_pz90 << " : " 
+        << sr_M_3_wgs84 << endl;
+
+
+
+
+
+
 
 
 
