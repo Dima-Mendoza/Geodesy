6eea7a89261a6a1bd9679ff8ec2233cca91a6d7c
Dima-Mendoza <spartanskijmapper534@gmail.com>
2024-11-09 23:48:34 +0300

    geodesy

--- bin/geodesy_four.cpp
+++ bin/geodesy_four.cpp
@@ -5,6 +5,13 @@
 
 int main() {
 
+    long double b = 44797.282+12*18;
+
+    long double A = 62.21257138888888888888;
+    long double B = 50.33904222222222222222;
+    long double C = 67.44991694444444444444;
+
+    
 
     return 0;
 }
